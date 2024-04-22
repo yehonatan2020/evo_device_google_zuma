5171d76fada610967452c0c72cd3ddb1b3821521
AnierinB <anierin@evolution-x.org>
2024-04-22 14:29:06 -0700

    zuma: Initialize for Evolution X 10.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ BoardConfigEvolution.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Evolution X Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BUILD_BROKEN_DUP_RULES := true
+
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+SPOOF_FIRST_API_LEVEL_32 := true
